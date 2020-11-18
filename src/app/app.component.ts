46b434f0a9dbd85cd69a74795fe16048f8ac560c
w91379137 <w913791379137@gmail.com>
2020-11-18 18:05:15 +0800

    新增 pixi js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'pixi-typescript';
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
+  ngOnInit(): void {
+
+    const app = new PIXI.Application({
+      width: 256,         // default: 800
+      height: 256,        // default: 600
+      antialias: true,    // default: false
+      transparent: false, // default: false
+      resolution: 1,       // default: 1
+      forceCanvas: true,
+    });
+    document.body.appendChild(app.view);
+    app.renderer.backgroundColor = 0x0616ff;
+  }
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
 }
