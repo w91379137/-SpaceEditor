c6f3bb386caa7aa7d576371c37e90ca7ae621d3c
w91379137 <w913791379137@gmail.com>
2020-11-19 17:32:24 +0800

    取代原本的 pixi view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PixiViewComponent } from './component/pixi-view/pixi-view.component';
 import { ControlCenterService } from './service/control-center/control-center.service';
-import { PixiControlViewComponent } from './pixi-control-view/pixi-control-view.component';
+import { PixiControlViewComponent } from './component/pixi-control-view/pixi-control-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PixiViewComponent,
     PixiControlViewComponent,
   ],
   imports: [
--- src/app/component/pixi-control-view/pixi-control-view.component.ts
+++ src/app/component/pixi-control-view/pixi-control-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ControlCenterService } from 'src/app/service/control-center/control-center.service';
+import { ControlPoint } from 'src/class/control-point';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-pixi-control-view',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PixiControlViewComponent implements OnInit {
 
-  constructor() { }
+  app: PIXI.Application;
 
-  ngOnInit() {
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
+  constructor(
+    private controlCenter: ControlCenterService,
+  ) {
+    this.controlCenter.onAddClickSubject.subscribe(() => {
+      this.onAddClick();
+    });
   }
 
+  ngOnInit(): void {
+
+    this.app = new PIXI.Application({
+      width: 256,         // default: 800
+      height: 256,        // default: 600
+      antialias: true,    // default: false
+      transparent: true, // default: false
+      resolution: 1,       // default: 1
+      forceCanvas: true,
+    });
+
+    document.querySelector('#pixi').appendChild(this.app.view);
+    this.resize(window.innerWidth, window.innerHeight);
+  }
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    const target = event.target;
+    this.resize(target.innerWidth, target.innerHeight);
+  }
+
+  resize(width, height) {
+    console.log(width, height);
+    this.app.renderer.resize(width, height);
+  }
+
+  // ====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====.====
+  onAddClick() {
+    const circle = new ControlPoint(this.app, 100, 100, 10, 0x333333);
+    this.controlCenter.controlPointArr.push(circle);
+  }
 }
